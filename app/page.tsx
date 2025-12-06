74145548c393401b876a4e4964a995567458e67e
jacoblee93 <jacoblee93@gmail.com>
2025-12-06 11:32:14 -0800

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Suspense } from "react";
 import { ChatWindow } from "@/components/ChatWindow";
 
 export default function Home() {
   return (
-    <ChatWindow placeholder="Try asking something about the document you just uploaded!"></ChatWindow>
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          Loading...
+        </div>
+      }
+    >
+      <ChatWindow placeholder="Try asking something about the document you just uploaded!"></ChatWindow>
+    </Suspense>
   );
 }
